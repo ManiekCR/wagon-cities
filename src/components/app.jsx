1cd558a427f1e8bbbcd3b2e917b499204d0b0807
Marian Caron <caron.marian@gmail.com>
2020-10-05 21:13:02 +0200

    add header + update color

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import CityList from '../containers/city_list';
 import ActiveCity from '../containers/active_city';
+import Header from './header';
 
 const App = () => {
   return (
-    <div className="app">
-      <CityList />
-      <ActiveCity />
+    <div>
+      <Header />
+      <div className="app">
+        <CityList />
+        <ActiveCity />
+      </div>
     </div>
   );
 };
---
+++ src/components/header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <h1 className="header">
+      Le Wagon&apos;s offices
+    </h1>
+  );
+};
+
+export default(Header);
